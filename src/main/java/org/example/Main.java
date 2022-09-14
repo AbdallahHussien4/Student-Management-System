d7b178e41822750e038c8e7647b51a076db6ecb3
Abdallah Hussien <abdallahhussien40@gmail.com>
2022-09-14 13:04:48 +0200

    Add 3 implementations of singleton design pattern

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,7 +23,6 @@ public class Main {
     public static void main(String[] args) throws ParseException, InterruptedException {
         Seeds.seed();
         boolean exit = false;
-        // TODO : User & UserUtil Interfaces.
         // TODO : Strategy Design Pattern
         while(true && !exit)
         {
--- src/main/java/org/example/Singleton.java
+++ src/main/java/org/example/Singleton.java
@@ -3,55 +3,64 @@ package org.example;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+
 public class Singleton {
-    private static Singleton singleton;
+    private static Singleton singleton = new Singleton(); //Eager
     private HashSet<Teacher> teachers;
     private HashSet<Student> students;
     private HashSet<Course> courses;
     private HashSet<AssignmentSubmission> assignmentSubmissions;
     private HashSet<CourseAttendance> courseAttendances;
 
-    private Singleton() {}
+    /* Enum Implementation
+      public enum Singleton {
+        singleton;
+        private Singleton() {
+            teachers = new HashSet<Teacher>();
+            students = new HashSet<Student>();
+            courses = new HashSet<Course>();
+            assignmentSubmissions = new HashSet<AssignmentSubmission>();
+            courseAttendances = new HashSet<CourseAttendance>();
+        }
+    }*/
+    private Singleton() {
+        teachers = new HashSet<Teacher>();
+        students = new HashSet<Student>();
+        courses = new HashSet<Course>();
+        assignmentSubmissions = new HashSet<AssignmentSubmission>();
+        courseAttendances = new HashSet<CourseAttendance>();
+    }
     public static Singleton getSingleton()
     {
-        //TODO : Syncronized
         //Enum
-        //Eagur
-        if (singleton == null)
-            singleton = new Singleton();
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
         return singleton;
     }
     public HashSet<Teacher> getTeachers()
     {
-        if (teachers == null)
-            teachers = new HashSet<Teacher>();
         return teachers;
     }
     public HashSet<Student> getStudents()
     {
-        if (students == null)
-            students = new HashSet<Student>();
         return students;
     }
-
-
     public HashSet<Course> getCourses()
     {
-        if (courses == null)
-            courses = new HashSet<Course>();
         return courses;
     }
 
     public HashSet<AssignmentSubmission> getAssignmentSubmissions()
     {
-        if (assignmentSubmissions == null)
-            assignmentSubmissions = new HashSet<AssignmentSubmission>();
         return assignmentSubmissions;
     }
     public HashSet<CourseAttendance> getCourseAttendances()
     {
-        if (courseAttendances == null)
-            courseAttendances = new HashSet<CourseAttendance>();
         return courseAttendances;
     }
 
